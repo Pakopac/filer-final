1e6b60770113f879b6dfa008cb4768c36c246a1c
sonam8848 <sonam.sherpa@supinternet.fr>
2018-03-06 23:05:37 +0100

    add it

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -1,11 +1,105 @@
 <?php
-
 require_once('Cool/BaseController.php');
-
+require_once('Model/RegisterManager.php');
+require_once('Model/LoginManager.php');
+require_once('Model/UploadManager.php');
+session_start();
 class MainController extends BaseController
 {
     public function homeAction()
     {
+        if(isset($_SESSION)) {
+            $data = ['user' => $_SESSION];
+            return $this->render('home.html.twig',$data);
+        }
+        return $this->render('home.html.twig');
+    }
+    public function loginAction()
+    {
+        if(!empty($_POST['loginPseudo']) && !empty($_POST['loginPassword'])) {
+            $pseudo = htmlentities($_POST['loginPseudo']);
+            $password = $_POST['loginPassword'];
+            $errors = 'Invalid pseudo or password';
+
+            $LoginManager = new LoginManager();
+            $LoginManager -> loginUser($pseudo, $password,$errors);
+            if(isset($_SESSION['pseudo'])) {
+                $data = ['user' => $_SESSION];
+                $this->redirectToRoute('home');
+                return $this->render('home.html.twig', $data);
+            }
+            else{
+                $data = ['errors' => $errors];
+                return $this->render('login.html.twig', $data);
+            }
+            }
+        return $this->render('login.html.twig');
+    }
+    public function registerAction()
+    {
+        if(!empty($_POST['firstname']) && !empty($_POST['lastname'])
+            && !empty($_POST['pseudo']) && !empty($_POST['email'])
+            && !empty($_POST['password']) && !empty($_POST['repeatPassword'])){
+            $firstname = htmlentities($_POST['firstname']);
+            $lastname = htmlentities($_POST['lastname']);
+            $pseudo = htmlentities($_POST['pseudo']);
+            $email = htmlentities($_POST['email']);
+            $password = $_POST['password'];
+            $repeatPassword = $_POST['repeatPassword'];
+
+            $regexPassword = "\"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$\"";
+            $regexEmail =  " /^[^\W][a-zA-Z0-9]+(.[a-zA-Z0-9]+)@[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*.[a-zA-Z]{2,4}$/ ";
+            $errors = [];
+
+            if (!preg_match($regexEmail,$email)){
+                $errors[] = 'Invalid Email';
+            }
+            if((strlen($pseudo) < 4) || (strlen($pseudo) > 20)){
+                $errors[] = 'Pseudo too short or too long';
+            }
+            if(!preg_match($regexPassword,$password)){
+                $errors[] = 'Password must have at least 6 characters with 1 letter uppercase and 1 number';
+            }
+            if($password !== $repeatPassword){
+                $errors[] = 'Password must be identical to the verification';
+            }
+
+            if($errors === []) {
+                $RegisterManager = new RegisterManager();
+                $RegisterManager->registerUser($firstname, $lastname, $pseudo, $email, $password);
+                $data = ['user' => $_SESSION];
+                $this->redirectToRoute('home');
+                return $this->render('layout.html.twig', $data);
+            }
+            else{
+                $data = ['errors' => $errors];
+                return $this->render('register.html.twig', $data);
+            }
+        }
+        return $this->render('register.html.twig');
+    }
+    public function UploadFilesAction()
+    {
+        if(isset($_FILES['inputFile'])){
+            $UploadManager = new UploadManager();
+            $messagesUpload = $UploadManager->uploadFiles();
+            $data = ['file' => $_FILES,
+                'user' => $_SESSION,
+                'messagesUpload' => $messagesUpload];
+            return $this->render('uploadFiles.html.twig',$data);
+        }
+        $data = ['user' => $_SESSION];
+        return $this->render('uploadFiles.html.twig',$data);
+    }
+    public function viewFilesAction()
+    {
+        $data = ['user' => $_SESSION];
+        return $this->render('viewFiles.html.twig',$data);
+    }
+    public function logoutAction()
+    {
+        session_destroy();
+        $this -> redirectToRoute('home');
         return $this->render('home.html.twig');
     }
 }
